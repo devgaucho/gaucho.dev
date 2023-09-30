85ce163c2d6a6baf8d96f14ebd94d66c3efd3490
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-09-30 02:17:09 -0300

    atualização do blog

--- view/logo.php
+++ view/logo.php
@@ -2,9 +2,7 @@
 	<a href="." title="Gaucho.dev">
 		<img class="circle" height="120" width="120" src="img/logo.jpg" alt="Gaucho.dev">
 	</a>
-	<br>
-	<b>Anderson</b>
-	<br>
+	<h1>Anderson</h1>
 	<small>web dev (php) &bull; dataist wannabe</small>
 	<?php require 'footer.php'; ?>
 </div>
