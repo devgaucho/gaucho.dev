3dd0732447e0c1f792a39330c92bbc0573379364
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-10-06 15:58:32 -0300

    atualização do blog

--- view/post.php
+++ view/post.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />	
 	<link rel="shortcut icon" href="img/logo.jpg" />
 	<meta property="og:image" content="img/logo.jpg" />
-	<meta name="twitter:title" content="Gaucho.dev" />
+	<meta name="twitter:title" content="<?php print htmlentities($title);?>" />
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:image" content="img/logo.jpg" />
 	<link rel="stylesheet" href="css/style.css" />
