af9d26445a6770404d3f3f10964fff78b2ddf03c
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-11-23 11:38:53 -0300

    atualização do blog

--- view/footer.php
+++ view/footer.php
@@ -2,6 +2,6 @@
 	<b>
 		<a href="https://github.com/devgaucho">GitHub</a>
 		&bull;
-		<a href="https://x.com/devgaucho">Twitter</a>
+		<a href="https://twitter.com/devgaucho">Twitter</a>
 	</b>
 </p>
