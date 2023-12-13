cf21a4e1e25aba9847bba37f56b7952cc9b920c5
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-12-13 11:52:33 -0300

    atualização do blog

--- view/header.php
+++ view/header.php
@@ -7,6 +7,6 @@
 	<meta name="twitter:title" content="<?php print htmlentities($title);?>" />
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:image" content="img/logo.jpg" />
-	<link rel="stylesheet" href="css/style.css?v8" />
+	<link rel="stylesheet" href="css/style.css?v9" />
 </head>
 <body>
