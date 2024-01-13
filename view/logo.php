16f7b9231a857eb6a31db4c376b7d55de427d611
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-01-13 19:00:44 -0300

    atualização do blog

--- view/logo.php
+++ view/logo.php
@@ -1,8 +1,8 @@
 <div class="center">
 	<a href="." title="Gaucho.dev">
-		<img class="circle" height="120" width="120" src="img/logo_460.png" alt="Gaucho.dev">
+		<img class="circle" height="120" width="120" src="img/logo_460.png" alt="Gaucho">
 	</a>
-	<h1>Gaucho.dev</h1>
+	<h1>Gaucho</h1>
 	<p>fazedor de sites 🐘 &bull; quero ser dataísta mas não sei como 🤷‍♂️</p>
 	<?php require 'footer.php'; ?>
 </div>
