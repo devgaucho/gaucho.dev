8801efb07c5e4bca50287d0e99bb070d4586be50
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-11-30 19:28:23 -0300

    atualização do blog

--- view/header.php
+++ view/header.php
@@ -6,7 +6,7 @@
 	<link rel="shortcut icon" href="img/logo_460.png" />
 	<meta property="og:image" content="img/logo_460.png" />
 	<meta name="twitter:title" content="<?php print htmlentities($title);?>" />
-	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:image" content="img/logo_460.png" />
 	<link rel="stylesheet" href="css/style.css?v14" />
 </head>
